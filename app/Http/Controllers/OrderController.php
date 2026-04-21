ad6c81f4d611beddb0bf9b6708331a2f9fd02035
Eliab <35658748+eliabsiji@users.noreply.github.com>
2026-04-21 17:35:44 +0100

    Update OrderController.php

--- app/Http/Controllers/OrderController.php
+++ app/Http/Controllers/OrderController.php
@@ -99,7 +99,7 @@ class OrderController extends Controller
     public function show($id)
     {
         $order = Order::with([
-            'user:id,first_name,last_name,email,phone',
+            'user:id,first_name,last_name,email,phone_number',
             'items.product',
             'shippingAddress',
             'billingAddress',
