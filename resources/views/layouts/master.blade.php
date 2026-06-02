2e35880bb21dad7cb0941705b3396ad066679051
Eliab <35658748+eliabsiji@users.noreply.github.com>
2026-06-03 00:58:52 +0100

    Update master.blade.php

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -1107,7 +1107,7 @@
          ===================================================== -->
     <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
     <script src="{{ asset('theme/layouts/assets/libs/bootstrap/js/bootstrap.bundle.min.js') }}"></script>
-    <script src="{{ asset('theme/layouts/assets/js/app.min.js') }}"></script>
+    <script src="{{ asset('theme/layouts/assets/js/app.js') }}"></script>
     <script src="{{ asset('theme/layouts/assets/libs/simplebar/simplebar.min.js') }}"></script>
     <script src="{{ asset('theme/layouts/assets/js/plugins.js') }}"></script>
     <script src="https://cdn.jsdelivr.net/npm/select2@4.1.0-rc.0/dist/js/select2.min.js"></script>
